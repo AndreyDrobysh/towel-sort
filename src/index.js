8b2c45f1aa61d64e4caef95dcb685405132b3ca6
Andrei Drobysh <andrey.drobysh.04@gmail.com>
2021-03-11 16:49:19 +0300

    fihish task

--- src/index.js
+++ src/index.js
@@ -2,5 +2,26 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
+  const compareNumeric = (a,b) => a - b;
+
+  const compareNumeric_1 = (a,b) => b - a;
+
+  const newArr = [];
+  if (!matrix) {
+    return newArr;
+  }
+  matrix.forEach(function (item, i) {
+    if (i % 2 === 0) {
+     matrix[i].sort(compareNumeric);
+    } else {
+      matrix[i].sort(compareNumeric_1)
+    }
+  })
+
+  for (let i = 0; i < matrix.length; i++) {
+    matrix[i].forEach(function (item, i) {
+      newArr.push(item);
+    })
+  }
+  return newArr;
 }
